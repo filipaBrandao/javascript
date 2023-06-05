ea7e4e3e8b24bce2eb3a1d7da4904b935b67b278
Filipa Brandão <filipabrandao0@icloud.com>
2023-06-05 09:38:10 -0300

    finalizacao modelo

--- aula-12ex/ex-14/script.js
+++ aula-12ex/ex-14/script.js
@@ -7,9 +7,12 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.png'
+        document.body.style.background = '#f5dec2'
     } else if (hora >= 12 && hora < 18) {
         img.src = 'fototarde.png'
+        document.body.style.background = '#df7b4e'
     } else {
-
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#29464a'
     }
 }
