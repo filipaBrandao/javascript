19da7d4dcc56edda14abaacaacf93017509861f4
Filipa Brandão <filipabrandao0@icloud.com>
2023-06-06 10:28:15 -0300

    repeticao for

---
+++ aula-14/ambiente.js
@@ -0,0 +1,25 @@
+/*
+var c = 1
+while (c <= 5 ) {
+    console.log(c)
+    c++
+}
+
+
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5 ) 
+
+
+for(var c = 1; c<= 5; c++) {
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c<= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
